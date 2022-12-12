6bb1ae2c56084dd94d567f80e0395fec51e2b8b2
Jonathan <souzajonathan570@gmail.com>
2022-12-12 10:00:15 -0300

    feat: adds new styles and the map

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,12 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="shotcut icon" href="/img/icon-192.png" />
         <link rel="apple-touch-icon" href="/img/icon-192.png" />
         <link rel="manisfest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        />
 
         <meta name="theme-color" content="#232946" />
         <meta
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,18 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --highlight: #ff8906;
+    --background: #0f0e17;
+    --white: #fffffe;
+
+    --container: 100rem;
+
+    --small: 1.5rem;
+    --medium: 3rem;
+    --large: 5rem;
+  }
+
   * {
     box-sizing: 0;
     margin: 0;
@@ -12,11 +24,23 @@ const GlobalStyles = createGlobalStyle`
   }
 
   html, body, #__next {
+    background-color: var(--background);
+    color: var(--white);
     height: 100%;
   }
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  p,
+  a {
+    font-size: 2rem;
+    line-height: var(--medium);
+  }
+
+  a {
+    color: var(--highlight);
   }
 `
 
